b06cfb77bb67873634ec38d04f7b90f17db6d02c
Joseph Wachira <washywachira@gmail.com>
2023-04-02 00:28:47 +0300

    Completed assignment

--- main.js
+++ main.js
@@ -4,6 +4,38 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const modal = document.querySelector('#modal');
+const modalMessage = document.querySelector('#modal-message');
+const likeButtons = document.querySelectorAll('.like');
+
+// Hide error modal on page load
+modal.classList.add('hidden');
+
+// Function to handle heart icon click
+function handleClick(event) {
+  const heart = event.target;
+  mimicServerCall()
+    .then(() => {
+      // On successful response, update heart icon and style
+      heart.innerText = FULL_HEART;
+      heart.classList.add('activated-heart');
+    })
+    .catch(() => {
+      // On error response, display modal with message
+      modalMessage.innerText = 'Random server error. Please try again.';
+      modal.classList.remove('hidden');
+      setTimeout(() => {
+        modal.classList.add('hidden');
+      }, 3000);
+    });
+}
+
+// Add event listener to all heart icons
+for (const button of likeButtons) {
+  button.addEventListener('click', handleClick);
+}
+
+
 
 
 
